abe9bf828718d1a2ce3eeac15c79fc3e032f19d7
ballin <a0955771728@gmail.com>
2018-12-02 16:39:12 +0800

    測試打包後noconsole會不會當掉

--- bar.py
+++ bar.py
@@ -8,27 +8,67 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(303, 102)
+        MainWindow.setEnabled(True)
+        MainWindow.resize(352, 78)
+        # MainWindow.setFixedSize(MainWindow.width(), MainWindow.height())
+        # MainWindow.setWindowFlags(QtCore.Qt.WindowMinimizeButtonHint)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setEnabled(True)
+        self.centralwidget.setMinimumSize(QtCore.QSize(0, 0))
+        self.centralwidget.setMaximumSize(QtCore.QSize(16777215, 16777215))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.centralwidget.setFont(font)
         self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.gridLayout_2 = QtWidgets.QGridLayout()
+        self.gridLayout_2.setObjectName("gridLayout_2")
         self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
-        self.progressBar.setGeometry(QtCore.QRect(10, 10, 171, 41))
+        self.progressBar.setStyleSheet(
+            "font: 63 18pt \"Bahnschrift SemiBold Condensed\";")
         self.progressBar.setProperty("value", 0)
         self.progressBar.setObjectName("progressBar")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(200, 20, 75, 23))
-        self.pushButton.setObjectName("pushButton")
+        self.gridLayout_2.addWidget(self.progressBar, 0, 0, 1, 1)
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setObjectName("gridLayout")
+        self.ok_job = QtWidgets.QLabel(self.centralwidget)
+        self.ok_job.setStyleSheet(
+            "font: 63 18pt \"Bahnschrift SemiBold Condensed\";")
+        self.ok_job.setObjectName("ok_job")
+        self.gridLayout.addWidget(self.ok_job, 0, 0, 1, 1)
+        self.splash = QtWidgets.QLabel(self.centralwidget)
+        self.splash.setStyleSheet(
+            "font: 63 18pt \"Bahnschrift SemiBold Condensed\";")
+        self.splash.setObjectName("splash")
+        self.gridLayout.addWidget(self.splash, 0, 1, 1, 1)
+        self.total_job = QtWidgets.QLabel(self.centralwidget)
+        self.total_job.setStyleSheet(
+            "font: 63 18pt \"Bahnschrift SemiBold Condensed\";")
+        self.total_job.setObjectName("total_job")
+        self.gridLayout.addWidget(self.total_job, 0, 2, 1, 1)
+        self.gridLayout_2.addLayout(self.gridLayout, 0, 2, 1, 1)
+        self.startButton = QtWidgets.QPushButton(self.centralwidget)
+        self.startButton.setStyleSheet(
+            "font: 63 18pt \"Bahnschrift SemiBold Condensed\";")
+        self.startButton.setObjectName("startButton")
+        self.gridLayout_2.addWidget(self.startButton, 0, 3, 1, 1)
+        self.exitButton = QtWidgets.QPushButton(self.centralwidget)
+        self.exitButton.setStyleSheet(
+            "font: 63 18pt \"Bahnschrift SemiBold Condensed\";")
+        self.exitButton.setObjectName("exitButton")
+        self.gridLayout_2.addWidget(self.exitButton, 0, 4, 1, 1)
+        self.horizontalLayout_2.addLayout(self.gridLayout_2)
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 303, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        self.menuBar = QtWidgets.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 352, 21))
+        self.menuBar.setObjectName("menuBar")
+        MainWindow.setMenuBar(self.menuBar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -36,5 +76,8 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "OK"))
-
+        self.ok_job.setText(_translate("MainWindow", "0"))
+        self.splash.setText(_translate("MainWindow", "/"))
+        self.total_job.setText(_translate("MainWindow", "0"))
+        self.startButton.setText(_translate("MainWindow", "Start"))
+        self.exitButton.setText(_translate("MainWindow", "Stop"))
--- bar_main.py
+++ bar_main.py
@@ -2,25 +2,122 @@
 # -*- coding: UTF-8 -*-
 
 import sys
+import time
+import requests
+import re
+import random
+from bs4 import BeautifulSoup
+from urllib.request import urlretrieve
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtWidgets import QMessageBox
 from bar import Ui_MainWindow
+
+
+class go_Thread(QThread):  # create a thread to avoid the ui dead
+    stop_signal = pyqtSignal()
+
+    def __init__(self, go):
+        QThread.__init__(self)
+        self.go = go
+        self.stop_flag = False
+
+    def stop(self):
+        self.stop_flag = True
+        self.run()
+
+    def run(self):
+        # print('stop_flag : {}'.format(self.stop_flag))
+        if self.stop_flag == False:
+            self.go()
+        else:
+            self.stop_signal.emit()
+
+
+class progress_Thread(QThread):  # create a thread to avoid the ui dead
+    update = pyqtSignal(int, int, int)
+
+    def __init__(self, bar_value, ok_job_count, total_job_count):
+        QThread.__init__(self)
+        self.bar_value = bar_value
+        self.ok_job_count = ok_job_count
+        self.total_job_count = total_job_count
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):  # 發送訊號
+        self.update.emit(self.bar_value, self.ok_job_count,
+                         self.total_job_count)
+
 
 class MyForm(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MyForm, self).__init__(parent)
         self.setupUi(self)
-        self.job_count = 10
-        self.ok_count = 0
-        self.pushButton.clicked.connect(self.run) #連結開始按鈕
-            
-    def run(self):
-        value = (self.ok_count / self.job_count) * 100 #設定進度條進度
-        self.progressBar.setValue(value)
-        self.ok_count += 1
-        
-        if self.ok_count > self.job_count:
-            sys.exit(0)
-        
+        self.setWindowTitle('ProgressBar-Test')
+        self.total_job_count = 10000
+        self.ok_job_count = 0
+        self.startButton.clicked.connect(self.start)  # 連結開始按鈕
+        # self.exitButton.clicked.connect(self.stop_work)  # stop thread
+
+    def go(self):
+        while True:
+            self.bar_value = (self.ok_job_count /
+                              self.total_job_count) * 100  # 設定進度條進度
+            try:
+                self._progress_Thread = progress_Thread(
+                    self.bar_value, self.ok_job_count, self.total_job_count)
+                self._progress_Thread.update.connect(
+                    self.set_labels)
+                self._progress_Thread.start()
+            except Exception as e:
+                print(e)
+
+            if self.ok_job_count == self.total_job_count:
+                self._progress_Thread.wait()  # 確定子進程都結束了才退出
+                self._progress_Thread.quit()
+                # 如果為 True，代表執行完 run 函式的時候，線程即退出
+                print(self._progress_Thread.isFinished())
+                print('The progress_Thread is finished!')
+                print('Done')
+                break
+            self.ok_job_count += 1
+
+    def start(self):
+        self.ok_job_count = 0
+        try:
+            self._go_Thread = go_Thread(
+                self.go)
+            # when stop_flag == True, all threads are done!
+            self._go_Thread.stop_signal.connect(self.done)
+            self._go_Thread.start()
+        except Exception as e:
+            print(e)
+
+    def done(self):
+        print(self._go_Thread.isFinished())
+        print('The go_Thread is finished!')
+        print('Done!')
+
+    def stop_work(self):  # 進入 stop 函數，go_thread 隨即停止。
+        self._go_Thread.stop()
+
+    # set progressBar and ok_job label and total_job label
+    def set_labels(self, bar_value, ok_job_count, total_job_count):
+        self.progressBar.setValue(bar_value)
+        self.ok_job.setText(str(ok_job_count))
+        self.total_job.setText(str(total_job_count))
+
+    def closeEvent(self, event):  # 退出事件
+        buttonReply = QMessageBox.question(self, '警告', '你確定要退出嗎?',
+                                           QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if buttonReply == QtWidgets.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     myapp = MyForm()
